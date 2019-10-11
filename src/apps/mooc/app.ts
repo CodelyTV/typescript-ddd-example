1b1d47bbfdbf606b18f53b569d1bddc47de17839
Alfredo Artiles <aartiles@gmail.com>
2019-10-11 13:22:56 +0200

    Add middlewares

--- src/apps/mooc/app.ts
+++ src/apps/mooc/app.ts
@@ -1,5 +1,7 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import helmet from 'helmet';
+import compress from 'compression';
 import { registerRoutes } from './infrastructure/routes';
 
 const app: express.Express = express();
@@ -8,6 +10,11 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action: 'deny' }));
+app.use(compress());
 
 registerRoutes(app);
 
