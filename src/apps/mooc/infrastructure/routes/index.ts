fbcf8e7552f617d680fc4f1802db57bd16f59d7a
rsaladocid <rsaladocid@gmail.com>
2019-10-16 19:09:51 +0200

    Reorganize apps folder

--- src/apps/mooc/infrastructure/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import {Express} from 'express';
-import { createUserRoute } from './create-course.route';
-
-export function registerRoutes(app: Express) {
-
-  createUserRoute(app);
-}
--- src/apps/mooc_backend/routes/create-course.route.ts
+++ src/apps/mooc_backend/routes/create-course.route.ts
@@ -1,9 +1,8 @@
 import { Express } from 'express';
-import container from '../../config/dependency-injection';
+import container from '../config/dependency-injection';
 import CreateCourseController from '../controllers/CreateCourseController';
 
 export const createUserRoute = (app: Express) => {
-
   const controller: CreateCourseController = container.get('Apps.mooc.controllers.CreateCourseController');
   app.post('/courses', (req, res) => controller.create(req, res));
 };
