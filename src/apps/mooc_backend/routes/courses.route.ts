f9720896b4c1ee5f1c63990baa5087594d890236
Fernando Vilas Maciel <vilasmaciel@gmail.com>
2019-11-08 17:42:36 +0100

    Auto configure routes using a file name pattern

--- src/apps/mooc_backend/routes/courses.route.ts
+++ src/apps/mooc_backend/routes/courses.route.ts
@@ -2,7 +2,7 @@ import { Express } from 'express';
 import container from '../config/dependency-injection';
 import CreateCourseController from '../controllers/CreateCourseController';
 
-export const createUserRoute = (app: Express) => {
+export const register = (app: Express) => {
   const controller: CreateCourseController = container.get('Apps.mooc.controllers.CreateCourseController');
-  app.put('/courses/:id', (req, res) => controller.create(req, res));
+  app.put('/courses/:id', controller.create.bind(controller));
 };
--- src/apps/mooc_backend/routes/status.route.ts
+++ src/apps/mooc_backend/routes/status.route.ts
@@ -2,7 +2,7 @@ import { Express } from 'express';
 import container from '../config/dependency-injection';
 import StatusController from '../controllers/StatusController';
 
-export const statusRoute = (app: Express) => {
+export const register = (app: Express) => {
   const controller: StatusController = container.get('Apps.mooc.controllers.StatusController');
-  app.get('/status', (req, res) => controller.create(req, res));
+  app.get('/status', controller.create.bind(controller));
 };
