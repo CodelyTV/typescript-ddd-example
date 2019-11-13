7cd747e65640bd3e9755502db0709b164c42edcd
rsaladocid <rsaladocid@gmail.com>
2019-11-13 18:42:50 +0100

    Refactor property access modifiers

--- src/Contexts/Mooc/Courses/domain/Course.ts
+++ src/Contexts/Mooc/Courses/domain/Course.ts
@@ -1,23 +1,11 @@
 export default class Course {
-  private _id: string;
-  private _name: string;
-  private _duration: string;
+  readonly id: string;
+  readonly name: string;
+  readonly duration: string;
 
   constructor(id: string, name: string, duration: string) {
-    this._id = id;
-    this._name = name;
-    this._duration = duration;
-  }
-
-  get id(): string {
-    return this._id;
-  }
-
-  get name(): string {
-    return this._name;
-  }
-
-  get duration(): string {
-    return this._duration;
+    this.id = id;
+    this.name = name;
+    this.duration = duration;
   }
 }
