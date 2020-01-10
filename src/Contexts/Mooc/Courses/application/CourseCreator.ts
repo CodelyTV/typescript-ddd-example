fa912d38ab6ec76477578de290761853c7e8aff3
rsaladocid <rsaladocid@gmail.com>
2020-01-10 10:47:26 +0100

    Use request/response objects

--- src/Contexts/Mooc/Courses/application/CourseCreator.ts
+++ src/Contexts/Mooc/Courses/application/CourseCreator.ts
@@ -1,15 +1,16 @@
-import CourseRepository from '../domain/CourseRepository';
-import Course from '../domain/Course';
+import { CourseRepository } from '../domain/CourseRepository';
+import { Course } from '../domain/Course';
+import { CreateCourseRequest } from './CreateCourseRequest';
 
-export default class CourseCreator {
+export class CourseCreator {
   private repository: CourseRepository;
 
   constructor(repository: CourseRepository) {
     this.repository = repository;
   }
 
-  async run(id: string, name: string, duration: string): Promise<void> {
-    const course = new Course(id, name, duration);
+  async run(request: CreateCourseRequest): Promise<void> {
+    const course = new Course(request.id, request.name, request.duration);
 
     return this.repository.save(course);
   }
--- tests/Contexts/Mooc/Courses/application/CourseCreator.test.ts
+++ tests/Contexts/Mooc/Courses/application/CourseCreator.test.ts
@@ -1,6 +1,6 @@
-import Course from '../../../../../src/Contexts/Mooc/Courses/domain/Course';
-import CourseCreator from '../../../../../src/Contexts/Mooc/Courses/application/CourseCreator';
-import CourseRepository from '../../../../../src/Contexts/Mooc/Courses/domain/CourseRepository';
+import { Course } from '../../../../../src/Contexts/Mooc/Courses/domain/Course';
+import { CourseCreator } from '../../../../../src/Contexts/Mooc/Courses/application/CourseCreator';
+import { CourseRepository } from '../../../../../src/Contexts/Mooc/Courses/domain/CourseRepository';
 
 describe('Course Creator', () => {
   it('should create a valid course', async () => {
@@ -18,7 +18,7 @@ describe('Course Creator', () => {
 
     const course = new Course(id, name, duration);
 
-    await createCourse.run(id, name, duration);
+    await createCourse.run({ id, name, duration });
 
     expect(save).toHaveBeenCalledWith(course);
   });
