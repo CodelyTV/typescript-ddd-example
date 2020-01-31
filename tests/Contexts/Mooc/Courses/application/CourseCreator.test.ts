c7b74114a427863dece2b16f70189803ce2bbdbc
Antonio Leon <antonioleon@audiense.com>
2020-01-31 15:17:29 +0100

    WIP simple approach with concerns as code comments

--- tests/Contexts/Mooc/Courses/application/CourseCreator.test.ts
+++ tests/Contexts/Mooc/Courses/application/CourseCreator.test.ts
@@ -6,159 +6,89 @@ import { EventBus } from '../../../../../src/Contexts/Shared/domain/EventBus';
 import { CourseCreator } from '../../../../../src/Contexts/Mooc/Courses/application/CourseCreator';
 import { Course } from '../../../../../src/Contexts/Mooc/Courses/domain/Course';
 import { CourseCreatedDomainEvent } from '../../../../../src/Contexts/Mooc/Courses/domain/CourseCreatedDomainEvent';
-import { AssertionError } from 'assert';
 import { DomainEvent } from '../../../../../src/Contexts/Shared/domain/DomainEvent';
-import { Domain } from 'domain';
+import { CourseId } from '../../../../../src/Contexts/Mooc/Shared/domain/Courses/CourseId';
 
-let repository: CourseRepository;
-let bus: EventBus;
+let repository: CourseRepositoryDouble;
+let bus: EventBusDouble;
 let creator: CourseCreator;
-declare global {
-  namespace jest {
-    interface Matchers<R> {
-      toPublish(a: DomainEvent, b: DomainEvent): R;
-    }
-  }
-}
-
-expect.extend({
-  toPublish(receivedEvent, expectedEvent) {
-    const comparator = new DomainEventComparator()
-    const pass = comparator.isSimilar(receivedEvent, expectedEvent);
-    if (pass) {
-      return {
-        message: () =>
-          `todo bien`,
-        pass: true,
-      };
-    } else {
-      return {
-        message: () =>
-          `expected ${receivedEvent} to be within range ${expectedEvent}`,
-        pass: false,
-      };
-    }
-  },
-});
-
-const createRepository = (): CourseRepository => ({ save: jest.fn(), search: jest.fn() });
-const eventBus = (): EventBusMock => (new EventBusMock());
-const shouldSave = (course: Course) => expect(repository.save).toHaveBeenCalledWith(course);
-const shouldPublishDomainEvent = (event: CourseCreatedDomainEvent) => expect((bus.publish as jest.Mock);
-
-beforeEach(() => {
-  repository = createRepository();
-  bus = eventBus();
-  creator = new CourseCreator(repository, bus);
-});
 
 describe('Course Creator', () => {
+  beforeEach(() => {
+    repository = new CourseRepositoryDouble();
+    bus = new EventBusDouble();
+    creator = new CourseCreator(repository, bus);
+  });
+
   it('should create a valid course', async () => {
     const request = CreateCourseRequestMother.random();
-
-    const course = CourseMother.fromRequest(request);
-    const event = CourseCreatedDomainEventMother.fromCourse(course);
+    const expectedCourse = CourseMother.fromRequest(request);
+    const expectedEvent = CourseCreatedDomainEventMother.fromCourse(expectedCourse);
 
     await creator.run(request);
 
-    //shouldSave(course);
-    shouldPublishDomainEvent(event);
+    shouldSave(expectedCourse);
+    shouldPublishDomainEvent(expectedEvent);
   });
 });
-class EventBusMock implements EventBus {
+
+const shouldSave = (course: Course) => {
+  // I am not sure at all if we should remove the recorded events before comparing
+  // I have done it to prevent the tests to fail if the courses have different events
+  // or events with different ids
+  expect(course).toMatchObject(repository.getLastSavedCourseWithoutRecordedEvents());
+};
+
+const shouldPublishDomainEvent = (event: CourseCreatedDomainEvent) => {
+  expect(bus.hasPublishedLastEvent(event)).toBe(true);
+};
+
+class EventBusDouble implements EventBus {
   private spy = jest.fn();
   publish(events: DomainEvent[]): void {
     this.spy(events);
   }
 
-  toHaveBeenCalledWith(event: DomainEvent) {
-    this.spy.toPublish(event);
+  hasPublishedLastEvent(event: DomainEvent) {
     const eventArg: DomainEvent[] = this.spy.mock.calls[0][0];
     return this.isSimilarTo(event, eventArg[0]);
   }
 
   private isSimilarTo(event1: DomainEvent, event2: DomainEvent) {
     const event1Data = this.getDomainAttributes(event1);
+    const event1DataKeys = Object.keys(event1Data);
     const event2Data = this.getDomainAttributes(event2);
+    const event2DataKeys = Object.keys(event2Data);
 
-    return expect(event1Data).toMatchObject(event2Data);
-  }
-
-  private getDomainAttributes(event: DomainEvent) {
-    const { eventId, occurredOn, ...data } = event;
-
-    return data;
-  }
-}
-
-class Mock {
-  toHaveBeenCalledWith(event: DomainEvent) {
-    const eventArg: DomainEvent[] = this.spy.mock.calls[0][0];
-    return this.isSimilarTo(event, eventArg[0]);
-  }
-
-  private isSimilarTo(event1: DomainEvent, event2: DomainEvent) {
-    const event1Data = this.getDomainAttributes(event1);
-    const event2Data = this.getDomainAttributes(event2);
-
-    return expect(event1Data).toMatchObject(event2Data);
-  }
-
-  private getDomainAttributes(event: DomainEvent) {
-    const { eventId, occurredOn, ...data } = event;
-
-    return data;
-  }
-}
-
-interface Comparator<T> {
-  isSimilar(t1: T, t2: T): void;
-}
-
-class DomainEventComparator implements Comparator<DomainEvent> {
-  isSimilar(event1: DomainEvent, event2: DomainEvent) {
-    const event1Data = this.getDomainAttributes(event1);
-    const event2Data = this.getDomainAttributes(event2);
-
-    return expect(event1Data).toMatchObject(event2Data);
-  }
-
-  private getDomainAttributes(event: DomainEvent) {
-    const { eventId, occurredOn, ...data } = event;
-
-    return data;
-  }
-}
-class DefaultComparator implements Comparator<any> {
-  isSimilar(event1: any, event2: any) {
-    return expect(event1).toMatchObject(event1);
-  }
-
-}
-class ComparatorFactory {
-  static comparators = new Map<string, Comparator<any>>();
-
-  static getComparatorFor(object: Object): Comparator<any> {
-    const name: any = object.constructor.name;
-
-    if (ComparatorFactory.comparators.has(name)) {
-      return ComparatorFactory.comparators.get(name)!;
+    if (event1DataKeys.length !== event2DataKeys.length) {
+      return false;
     }
+    // We are considering that all the info in the event are key: stringValue but
+    // the value may be an object and then the following comparation is not correct
+    return event1DataKeys.every(key => event1Data[key] === event2Data[key]);
+  }
 
-    return new DefaultComparator();
+  // I am not satisfied with the following "any"
+  private getDomainAttributes(event: DomainEvent): any {
+    const { eventId, occurredOn, ...data } = event;
+
+    return data;
   }
 }
 
-// function areSimilar(obj1: Object, obj2: Object, excludingFields: Array<string>) {
-//   const obj1WithoutExcludingFields = {};
-//   const obj2WithoutExcludingFields = {};
+class CourseRepositoryDouble implements CourseRepository {
+  private spySave: jest.Mock = jest.fn();
 
-//   return expect(obj1WithoutExcludingFields).toMatchObject(obj2WithoutExcludingFields);
-// }
+  async save(course: Course) {
+    this.spySave(course);
+  }
 
-// function pickProperties(obj: Object, excludingFields: Array<string>) {
-//   return obj.keys.reduce(function(keyName, value) {
-//     if (excludingFields.includes(keyName)) {
-//     }
-//   }, {});
-// }
+  getLastSavedCourseWithoutRecordedEvents() {
+    const lastSavedCourse = repository.spySave.mock.calls[0][0];
+    return new Course(lastSavedCourse.id, lastSavedCourse.name, lastSavedCourse.duration);
+  }
+
+  async search(id: CourseId) {
+    return null;
+  }
+}
