08ffb748fc952e9fd9039649fe3e364876c6e87b
rsaladocid <rsaladocid@gmail.com>
2020-02-06 08:52:41 +0100

    Add new unit test

--- tests/Contexts/Shared/infrastructure/MongoClientFactory.test.ts
+++ tests/Contexts/Shared/infrastructure/MongoClientFactory.test.ts
@@ -1,7 +1,7 @@
 import { MongoClientFactory } from '../../../../src/Contexts/Shared/infrastructure/persistence/mongo/MongoClientFactory';
 import { MongoClient } from 'mongodb';
 
-describe('MongoClientFactory', () => {
+describe('Create a client', () => {
   const factory = MongoClientFactory;
   let client: MongoClient;
 
@@ -13,12 +13,24 @@ describe('MongoClientFactory', () => {
     await client.close();
   });
 
-  it('creates a new client if it does not exist', () => {
+  it('creates a new client with the connection already established', () => {
     expect(client).toBeInstanceOf(MongoClient);
+    expect(client.isConnected()).toBeTruthy();
   });
 
-  it('creates a new client with existing name should return existing client', async () => {
+  it('creates a new client if it does not exist a client with the given name', async () => {
+    const newClient = await factory.createClient('test2');
+
+    expect(newClient).not.toBe(client);
+
+    await newClient.close();
+  });
+
+  it('returns a client if it already exists', async () => {
     const newClient = await factory.createClient('test');
+
     expect(newClient).toBe(client);
+
+    await newClient.close();
   });
 });
