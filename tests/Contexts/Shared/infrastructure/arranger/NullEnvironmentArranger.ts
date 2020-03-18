ee5989ef83652a632c8d3a02d5c18de3bc967345
rsaladocid <rsaladocid@gmail.com>
2020-03-18 19:52:09 +0100

    Remove NullEnvironmentArranger

--- tests/Contexts/Shared/infrastructure/arranger/NullEnvironmentArranger.ts
+++
@@ -1,7 +0,0 @@
-import { EnvironmentArranger } from './EnvironmentArranger';
-
-export class NullEnvironmentArranger extends EnvironmentArranger {
-  protected async clean(name: string): Promise<void> {}
-
-  public async close(): Promise<void> {}
-}
