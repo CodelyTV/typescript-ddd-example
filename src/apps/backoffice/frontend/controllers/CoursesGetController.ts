297eb3dae9d478eb2304145decb571c9beffb5ad
David Matas <adavidmatas@gmail.com>
2020-07-03 16:51:58 +0200

    Eliminate 0 case from controller

--- src/apps/backoffice/frontend/controllers/CoursesGetController.ts
+++ src/apps/backoffice/frontend/controllers/CoursesGetController.ts
@@ -5,18 +5,12 @@ export class CoursesGetController {
   constructor(private coursesCounterFinder: CoursesCounterFinder) {}
 
   async run(req: Request, res: Response) {
-    let coursesNumber;
-    try {
-      const courses = await this.coursesCounterFinder.run();
-      coursesNumber = courses.total;
-    } catch (error) {
-      coursesNumber = 0;
-    }
+    const courses = await this.coursesCounterFinder.run();
 
     res.render('pages/courses', {
       title: 'Welcome',
       description: 'CodelyTV - Backoffice',
-      courses_counter: coursesNumber,
+      courses_counter: courses.total,
       new_course_id: 'xxxx'
     });
   }
