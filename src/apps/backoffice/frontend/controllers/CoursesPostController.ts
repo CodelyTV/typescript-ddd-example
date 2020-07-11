331b8dc35d126f5125833418370d4a25e5477794
Fran Ortiz <fortizabril@gmail.com>
2020-07-11 12:33:37 +0200

    Use command and query bus in backoffice frontend app

--- src/apps/backoffice/frontend/controllers/CoursesPostController.ts
+++ src/apps/backoffice/frontend/controllers/CoursesPostController.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
-import { CreateCourseRequest } from '../../../../Contexts/Mooc/Courses/application/CreateCourseRequest';
-import { CourseCreator } from '../../../../Contexts/Mooc/Courses/application/CourseCreator';
+import { CommandBus } from '../../../../Contexts/Shared/domain/CommandBus';
+import { CreateCourseCommand } from '../../../../Contexts/Mooc/Courses/application/CreateCourseCommand';
 
 export class CoursesPostController {
-  constructor(private courseCreator: CourseCreator) {}
+  constructor(private commandBus: CommandBus) {}
 
   async run(req: Request, res: Response) {
     // TODO: validation
@@ -13,11 +13,13 @@ export class CoursesPostController {
   }
 
   private async createCourse(req: Request, res: Response) {
-    await this.courseCreator.run({
-      courseId: req.body.id,
-      courseName: req.body.name,
-      courseDuration: req.body.duration
+    const createCourseCommand = new CreateCourseCommand({
+      id: req.body.id,
+      name: req.body.name,
+      duration: req.body.duration
     });
+    await this.commandBus.dispatch(createCourseCommand);
+
     req.flash('message', `Felicidades, el curso ${req.body.name} ha sido creado!`);
     res.redirect('/courses');
   }
