f4a71f20cf2a856747487c8bdd58ebf6bf9d82b5
Iván Portillo <ivanportillo@me.com>
2020-10-24 01:10:01 +0200

    Implement MongoBackofficeCourseRepository

---
+++ tests/Contexts/Backoffice/infrastructure/MongoBackofficeCourseRepository.test.ts
@@ -0,0 +1,25 @@
+import container from '../../../../src/apps/backoffice/backend/config/dependency-injection';
+import { MongoBackofficeCourseRepository } from '../../../../src/Contexts/Backoffice/infrastructure/MongoBackofficeCourseRepository';
+import { EnvironmentArranger } from '../../Shared/infrastructure/arranger/EnvironmentArranger';
+import { BackofficeCourseMother } from '../application/domain/BackofficeCourseMother';
+
+const repository: MongoBackofficeCourseRepository = container.get('Backoffice.Backend.courses.BackofficeCourseRepository');
+const environmentArranger: Promise<EnvironmentArranger> = container.get('Backoffice.Backend.EnvironmentArranger');
+
+beforeEach(async () => {
+  await (await environmentArranger).arrange();
+});
+
+afterAll(async () => {
+  await (await environmentArranger).close();
+});
+
+describe('Search all courses', () => {
+  it('should return the existing courses', async () => {
+    const courses = [BackofficeCourseMother.random(), BackofficeCourseMother.random(), BackofficeCourseMother.random()];
+
+    await Promise.all(courses.map(course => repository.save(course)));
+    
+    expect(courses).toEqual(await repository.searchAll());
+  });
+});
