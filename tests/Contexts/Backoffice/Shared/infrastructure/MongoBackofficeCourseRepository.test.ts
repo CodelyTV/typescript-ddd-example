da3af255b2c43ecb0a7af110c7afc95584b1a5b6
David Matas <adavidmatas@gmail.com>
2020-11-22 13:08:47 +0100

    Sort courses

--- tests/Contexts/Backoffice/Shared/infrastructure/MongoBackofficeCourseRepository.test.ts
+++ tests/Contexts/Backoffice/Shared/infrastructure/MongoBackofficeCourseRepository.test.ts
@@ -1,4 +1,5 @@
 import container from '../../../../../src/apps/backoffice/backend/config/dependency-injection';
+import { BackofficeCourse } from '../../../../../src/Contexts/Backoffice/Courses/domain/BackofficeCourse';
 import { MongoBackofficeCourseRepository } from '../../../../../src/Contexts/Backoffice/Shared/infrastructure/MongoBackofficeCourseRepository';
 import { EnvironmentArranger } from '../../../Shared/infrastructure/arranger/EnvironmentArranger';
 import { BackofficeCourseMother } from '../../Courses/domain/BackofficeCourseMother';
@@ -7,6 +8,10 @@ const repository: MongoBackofficeCourseRepository = container.get(
   'Backoffice.Backend.courses.BackofficeCourseRepository'
 );
 const environmentArranger: Promise<EnvironmentArranger> = container.get('Backoffice.Backend.EnvironmentArranger');
+
+function sort(backofficeCourse1: BackofficeCourse, backofficeCourse2: BackofficeCourse): number {
+  return backofficeCourse1?.id?.value.localeCompare(backofficeCourse2?.id?.value);
+}
 
 beforeEach(async () => {
   await (await environmentArranger).arrange();
@@ -23,7 +28,7 @@ describe('Mongo BackofficeCourse Repository', () => {
     await Promise.all(courses.map(course => repository.save(course)));
 
     const expectedCourses = await repository.searchAll();
-    expect(courses.sort()).toStrictEqual(expectedCourses.sort());
+    expect(courses.sort(sort)).toStrictEqual(expectedCourses.sort(sort));
   });
 
   it('should save a course', async () => {
