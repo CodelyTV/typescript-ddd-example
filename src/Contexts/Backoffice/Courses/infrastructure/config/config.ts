101e738bc05b256db7b05ce9916bf971668ebf26
Fernando Vilas Maciel <vilasmaciel@gmail.com>
2020-11-28 15:44:26 +0100

    Move Backoffice backend config to Backoffice context

--- src/Contexts/Backoffice/Courses/infrastructure/config/config.ts
+++ src/Contexts/Backoffice/Courses/infrastructure/config/config.ts
@@ -7,14 +7,6 @@ const convictConfig = convict({
     default: 'default',
     env: 'NODE_ENV'
   },
-  mongo: {
-    url: {
-      doc: 'The Mongo connection URL',
-      format: String,
-      env: 'MONGO_URL',
-      default: 'mongodb://localhost:27017/backoffice-backend-dev'
-    }
-  },
   elastic: {
     url: {
       doc: 'The Elastic connection URL',
