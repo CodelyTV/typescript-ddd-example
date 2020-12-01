707e9b44f731762cbe72982cbee48a04ae2de0f6
Manuel Gutiérrez <mgzamora@gmail.com>
2020-12-01 20:34:58 +0000

    added get course route

--- src/apps/mooc_backend/routes/courses.route.ts
+++ src/apps/mooc_backend/routes/courses.route.ts
@@ -5,6 +5,9 @@ export const register = (router: Router) => {
   const coursePutController = container.get('Apps.mooc.controllers.CoursePutController');
   router.put('/courses/:id', (req: Request, res: Response) => coursePutController.run(req, res));
 
+  const courseGetController = container.get('Apps.mooc.controllers.CourseGetController');
+  router.get('/courses/:id', (req: Request, res: Response) => courseGetController.run(req, res));
+
   const coursesCounterGetController = container.get('Apps.mooc.controllers.CoursesCounterGetController');
   router.get('/courses-counter', (req: Request, res: Response) => coursesCounterGetController.run(req, res));
 };
