8fc6dc8d2be3efff651d8543128646e0ca3e94aa
Manuel Gutiérrez <mgzamora@gmail.com>
2020-12-01 20:34:45 +0000

    added get controller

---
+++ src/apps/mooc_backend/controllers/CourseGetController.ts
@@ -0,0 +1,26 @@
+import { Controller } from './Controller';
+import { Request, Response } from 'express';
+import httpStatus = require('http-status');
+
+import { QueryBus } from '../../../Contexts/Shared/domain/QueryBus';
+import { GetCourseQuery } from '../../../Contexts/Mooc/Courses/application/GetCourse/GetCourseQuery';
+import { GetCourseResponse } from '../../../Contexts/Mooc/Courses/application/GetCourse/GetCourseResponse';
+import { CourseNotFound } from '../../../Contexts/Mooc/Courses/domain/CourseNotFound';
+
+export class CourseGetController implements Controller {
+  constructor(private queryBus: QueryBus) {}
+  async run(req: Request, res: Response): Promise<void> {
+    try {
+        const id: string = req.params.id;
+        const query = new GetCourseQuery({id});
+        const course = await this.queryBus.ask<GetCourseResponse>(query);
+        res.status(httpStatus.OK).send(course);
+    } catch (e) {
+      if (e instanceof CourseNotFound) {
+        res.status(httpStatus.NOT_FOUND).send();
+      } else {
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send();
+      }
+    }
+  }
+}
