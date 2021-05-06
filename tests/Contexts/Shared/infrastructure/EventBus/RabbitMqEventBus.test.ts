7c2f71db452861eedece26e8698223e064b1f58b
Fran Ortiz <fortizabril@gmail.com>
2021-05-06 15:32:27 +0200

    RabbitMqEventBus test: Use config from config factory

--- tests/Contexts/Shared/infrastructure/EventBus/RabbitMqEventBus.test.ts
+++ tests/Contexts/Shared/infrastructure/EventBus/RabbitMqEventBus.test.ts
@@ -1,13 +1,13 @@
 import { Uuid } from '../../../../../src/Contexts/Shared/domain/value-object/Uuid';
 import { DomainEventMapping } from '../../../../../src/Contexts/Shared/infrastructure/EventBus/DomainEventMapping';
-import RabbitMqConfig from '../../../../../src/Contexts/Shared/infrastructure/EventBus/RabbitMq/RabbitMqConfig';
 import RabbitMqEventbus from '../../../../../src/Contexts/Shared/infrastructure/EventBus/RabbitMq/RabbitMqEventBus';
+import { RabbitMqConfigFactory } from '../../../../../src/Contexts/Mooc/Shared/infrastructure/EventBus/RabbitMq/RabbitMqConfigFactory';
 import Logger from '../../../../../src/Contexts/Shared/infrastructure/WinstonLogger';
 import { DummyEvent } from './doubles/DummyEvent';
 import { SpyEventSubscriber } from './doubles/SpyEventSubscriber';
 
 describe('RabbitMqEventBus', () => {
-  const eventBus = aRabbitMqEventBus();
+  const eventBus = new RabbitMqEventbus(RabbitMqConfigFactory.createConfig(), new Logger());
   const subscriberSpy = new SpyEventSubscriber(DummyEvent);
 
   beforeAll(() => {
@@ -19,8 +19,8 @@ describe('RabbitMqEventBus', () => {
   it('publish and consume domain events', async () => {
     const event = new DummyEvent(Uuid.random().value);
 
-    await eventBus.publish([event]);
     await eventBus.start();
+    await eventBus.publish([event]);
 
     await waitUntil(() => subscriberSpy.numberOfReceivedEvents() === 1);
   });
@@ -30,26 +30,14 @@ async function waitUntil(condition: Function): Promise<void> {
   return new Promise((resolve, reject) => {
     const timeWas = new Date();
     const timeoutMs = 5000;
-    const wait = setInterval(function () {
+    const wait = setInterval(() => {
       if (condition()) {
         clearInterval(wait);
         return resolve();
       } else if (Number(new Date()) - Number(timeWas) > timeoutMs) {
         clearInterval(wait);
-        return reject();
+        return reject('Event was not consumed');
       }
     }, 20);
   });
-}
-
-function aRabbitMqEventBus() {
-  const rabbitMqConfig: RabbitMqConfig = {
-    host: 'localhost',
-    user: 'guest',
-    password: 'guest',
-    queue: 'testQueue',
-    exchange: 'testExchange'
-  };
-
-  return new RabbitMqEventbus(rabbitMqConfig, new Logger());
 }
