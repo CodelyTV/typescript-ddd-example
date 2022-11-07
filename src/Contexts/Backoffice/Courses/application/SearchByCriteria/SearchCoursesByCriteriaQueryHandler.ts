1054baa0b4fe19fff6ef8f5b8e0f5f1ab679d593
Cristian <coal182@gmail.com>
2022-11-07 17:00:41 +0100

    fix: arguments order switching limit and offset
    L22 limit and offset arguments were switched so the searcher was using the limit value instead of offset.

--- src/Contexts/Backoffice/Courses/application/SearchByCriteria/SearchCoursesByCriteriaQueryHandler.ts
+++ src/Contexts/Backoffice/Courses/application/SearchByCriteria/SearchCoursesByCriteriaQueryHandler.ts
@@ -19,6 +19,6 @@ export class SearchCoursesByCriteriaQueryHandler
     const filters = Filters.fromValues(query.filters);
     const order = Order.fromValues(query.orderBy, query.orderType);
 
-    return this.searcher.run(filters, order, query.offset, query.limit);
+    return this.searcher.run(filters, order, query.limit, query.offset);
   }
 }
